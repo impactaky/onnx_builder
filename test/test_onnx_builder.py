61e5d30d985336bc038778b871a43e730a469c1b
impactaky <impactaky@users.noreply.github.com>
2021-02-11 17:29:36 +0900

    Remove testcase that can't run with onnxruntime

--- test/test_onnx_builder.py
+++ test/test_onnx_builder.py
@@ -28,10 +28,58 @@ def test_from_onnx(onnx_file):
 
 
 onnx_runtime_fail_list = [
-    "test_gradient_of_add_and_mul",
-    "test_gradient_of_add",
+# See broken_test in
+# https://github.com/microsoft/onnxruntime/blob/master/onnxruntime/test/onnx/main.cc
+    "test_BERT_Squad",
+    "test_constantofshape_float_ones",
+    "test_constantofshape_int_zeros",
+    "test_cast_STRING_to_FLOAT",
+    "test_cast_FLOAT_to_STRING",
+    "test_tf_nasnet_large",
+    "test_tf_nasnet_mobile",
+    "test_tf_pnasnet_large",
+    "test_shrink",
+    "test_maxpool_with_argmax_2d_precomputed_strides",
+    "test_tf_inception_v2",
+    "test_tf_resnet_v1_50",
+    "test_tf_resnet_v1_101",
+    "test_tf_resnet_v1_152",
+    "test_mxnet_arcface",
+    "test_unique_not_sorted_without_axis",
+    "test_cumsum_1d_reverse_exclusive",
+    "test_resize_downsample_scales_cubic_align_corners",
+    "test_resize_downsample_scales_linear_align_corners",
+    "test_resize_tf_crop_and_resize",
+    "test_resize_upsample_sizes_nearest_ceil_half_pixel",
+    "test_resize_upsample_sizes_nearest_floor_align_corners",
+    "test_resize_upsample_sizes_nearest_round_prefer_ceil_asymmetric",
+    "test_bitshift_right_uint16",
     "test_bitshift_left_uint16",
+    "test_maxunpool_export_with_output_shape",
+    "test_training_dropout",
+    "test_training_dropout_default",
+    "test_training_dropout_default_mask",
+    "test_training_dropout_mask",
     "test_adagrad",
+    "test_adagrad_multiple",
+    "test_adam",
+    "test_adam_multiple",
+    "test_gradient_of_add",
+    "test_gradient_of_add_and_mul",
+    "test_momentum",
+    "test_momentum_multiple",
+    "test_nesterov_momentum",
+    "test_cast_FLOAT_to_BFLOAT16",
+    "test_cast_BFLOAT16_to_FLOAT",
+    "test_sequence_insert_at_back",
+    "test_sequence_insert_at_front",
+    "test_loop13_seq",
+# Other test gave the not implmented error
+    # Min(13)
+    "test_min_int8",
+    "test_min_uint8",
+    # Pow(13)
+    "test_pow_types_float32_uint32",
 ]
 test_cases = onnx_files
 test_cases = [
