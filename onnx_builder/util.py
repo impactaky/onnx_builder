044e44511ada15a3f1dc52e44d59d396ceff8b13
impactaky <impactaky@users.noreply.github.com>
2021-02-14 15:44:58 +0900

    Support load sequence.pb

--- onnx_builder/util.py
+++ onnx_builder/util.py
@@ -30,46 +30,53 @@ def value_info_to_numpy_info(tp: onnx.TensorProto):
     return (shape, elem_type)
 
 
-def get_uninitialized_input_names(model):
-    initializers = [x.name for x in model.graph.initializer]
-    input_names = []
-    for input_ in model.graph.input:
-        if input_.name in initializers:
-            continue
-        input_names.append(input_.name)
-    return input_names
+def _impl_load_pb(pb, type_, names):
+    f = open(pb, "rb")
+    if type_ == "tensor_type":
+        value = onnx.TensorProto()
+    elif type_ == "sequence_type":
+        value = onnx.SequenceProto()
+    elif type_ == "map_type":
+        value = onnx.MapProto()
+    value.ParseFromString(f.read())
+    if value.name not in names:
+        value.name = names[0]
+    return value
+
+
+def load_pbs(pbs, vis):
+    values = {}
+    names = [x.name for x in vis]
+    vis = {x.name: x for x in vis}
+    for pb in pbs:
+        tensor = _impl_load_pb(pb, "tensor_type", names)
+        value_type = vis[tensor.name].type.WhichOneof("value")
+        # TODO support map type
+        if value_type == "tensor_type":
+            values[tensor.name] = onnx.numpy_helper.to_array(tensor)
+        elif value_type == "sequence_type":
+            seq = _impl_load_pb(pb, value_type, names)
+            if vis[seq.name].type.WhichOneof("value") != value_type:
+                raise RuntimeError("Failure load {}".format(pb))
+            values[tensor.name] = onnx.numpy_helper.to_list(seq)
+        else:
+            raise RuntimeError("Unsupported value_type: {}".format(value_type))
+        if tensor.name in names:
+            names.remove(tensor.name)
+        else:
+            names.pop(0)
+    return values
 
 
 def load_inputs_from_test_case(test_case_dir, test_case_name="test_data_set_0"):
     test_case_dir = Path(test_case_dir)
     model = onnx.load(test_case_dir / "model.onnx")
-    input_names = get_uninitialized_input_names(model)
     input_pbs = glob.glob(str(test_case_dir / test_case_name / "input_*.pb"))
-    inputs = {}
-    for input_pb in input_pbs:
-        with open(input_pb, "rb") as f:
-            tensor = onnx.load_tensor(f)
-        if tensor.name in input_names:
-            input_names.remove(tensor.name)
-        else:
-            tensor.name = input_names.pop(0)
-        inputs[tensor.name] = onnx.numpy_helper.to_array(tensor)
-    return inputs
+    return load_pbs(input_pbs, model.graph.input)
 
 
 def load_outputs_from_test_case(test_case_dir, test_case_name="test_data_set_0"):
     test_case_dir = Path(test_case_dir)
     model = onnx.load(test_case_dir / "model.onnx")
-    output_names = [x.name for x in model.graph.output]
     output_pbs = glob.glob(str(test_case_dir / test_case_name / "output_*.pb"))
-    outputs = {}
-    for output_pb in output_pbs:
-        with open(output_pb, "rb") as f:
-            tensor = onnx.TensorProto()
-            tensor.ParseFromString(f.read())
-        if tensor.name in output_names:
-            output_names.remove(tensor.name)
-        else:
-            tensor.name = output_names.pop(0)
-        outputs[tensor.name] = onnx.numpy_helper.to_array(tensor)
-    return outputs
+    return load_pbs(output_pbs, model.graph.output)
