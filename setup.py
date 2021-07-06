f249bb13775d9ee8946479ea104cabec7a54e182
impactaky <impactaky@users.noreply.github.com>
2021-07-06 23:04:09 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.0.2",
     install_requires=["numpy", "onnx"],
     packages=find_packages(),
-    url=https://github.com/impactaky/onnx_builder
+    url="https://github.com/impactaky/onnx_builder"
 )
